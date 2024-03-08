f088ad492647f74bd84f445c6012c99278b27286
EmoKermitSSB <celyan.frelat@gmail.com>
2024-03-08 13:26:36 +0100

    Commit For Damage Cube and Repair Jump And isGrounded

---
+++ Assets/Proto/Code/MonsterDamage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDamage : MonoBehaviour
+{
+    public int damage;
+    public PlayerHealth playerHealth;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag ("Player"))
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
--- Assets/Proto/Code/movement.cs
+++ Assets/Proto/Code/movement.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField] int Speed = 5;
-    [SerializeField]bool isGrounded= false;
+    [SerializeField] int Speed;
+    [SerializeField] int DashSpeed;
+    [SerializeField] bool isGrounded = false;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] int JumpingPower;
+    [SerializeField] Transform GroundCheckCollider;
+    [SerializeField] const float GroundCheckRadius = 0.2f;
+    [SerializeField] LayerMask GroundLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,39 +22,65 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GroundCheck();
         moving();
     }
 
-
-    //Moove Mechanics
-/*private void OnCollisionEnter2D(Collision2D collision)
-{
-    if(collision)
+    void GroundCheck()
     {
+        isGrounded = false;
 
-    }
-}*/
-void moving()
-{
-    //if (isGrounded)
-    {
-
-
-     if (Input.GetKey(KeyCode.A))
-    {
-        transform.Translate(-Speed * Time.deltaTime, 0, 0);
-    }
-    if (Input.GetKey(KeyCode.S))
-    {
-        transform.Translate(0 * Time.deltaTime, 0, 0);
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-        transform.Translate(Speed * Time.deltaTime, 0, 0);
-
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(GroundCheckCollider.position, GroundCheckRadius, GroundLayer);
+        if(colliders.Length > 0)
+            isGrounded = true;
     }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("World"))
+        { 
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("World"))
+        {
+            isGrounded = false;
+        }
+    }
+
+
+
+
+    void moving()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+           //Attack Phisyc
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Speed * Time.deltaTime, 0, 0);
+        }
     }
 }
+    
+       
+  
+       
+   
 
-}
+
+
+
+
