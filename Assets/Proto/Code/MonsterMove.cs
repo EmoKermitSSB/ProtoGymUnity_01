d5cbb341d337edd9372abfa37578b6b4575ae28f
EmoKermitSSB <celyan.frelat@gmail.com>
2024-03-11 15:22:40 +0100

    Ennemis is moving !!! === repairing jump

---
+++ Assets/Proto/Code/MonsterMove.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
+public class MonsterMovement : MonoBehaviour
+{
+
+    [SerializeField] float Speed;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    public GameObject P_A;
+
+    public GameObject P_B;
+
+    [SerializeField] private Transform currentPoint;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = P_A.transform;
+    }
+
+
+    void Update()
+    {
+        Patrolling();
+    }
+
+
+
+    //All the movement Code
+    void Patrolling()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == P_B.transform)
+        {
+            rb.velocity = new Vector2(Speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-Speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == P_B.transform)
+        {
+            currentPoint = P_A.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == P_A.transform)
+        {
+            currentPoint = P_B.transform;
+        }
+    }
+}
+    
+       
+  
+       
+   
+
+
+
+
+
--- Assets/Proto/Code/PlayerHealth.cs
+++ Assets/Proto/Code/PlayerHealth.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] public GameObject[] Heart;
     [SerializeField] public int maxHealth;
     [SerializeField] public int health;
-
+    
     void Start()
     {
         health = maxHealth;
     }
     void Update()
     {
+        //UI hearth and health system 
             if (health < 1)
             {
                 
                 Destroy(Heart[0].gameObject);
+
+                if (health <= 0)
+            {
                 Destroy(gameObject);
+                SceneManager.LoadScene("Gym");
+
+            }
+                
             }
             else if (health < 2)
             {
@@ -31,9 +40,11 @@ public class PlayerHealth : MonoBehaviour
                 Destroy(Heart[2].gameObject);
                 
             }
+
+            
         }
     
-
+    //TakeDamages in fonction of the monster with the code MonsterDamage
     public void TakeDamage(int damage)
     {
         health -= damage;
