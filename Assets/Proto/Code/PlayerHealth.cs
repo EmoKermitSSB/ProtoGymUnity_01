7586ab280282d4bf9727367b5a63fe0cf5404c68
EmoKermitSSB <celyan.frelat@gmail.com>
2024-03-11 09:39:09 +0100

    Fixing Healing system testing double jumps

--- Assets/Proto/Code/PlayerHealth.cs
+++ Assets/Proto/Code/PlayerHealth.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int maxHealth;
-    public int health;
+    [SerializeField] public GameObject[] Heart;
+    [SerializeField] public int maxHealth;
+    [SerializeField] public int health;
 
     void Start()
     {
         health = maxHealth;
     }
+    void Update()
+    {
+            if (health < 1)
+            {
+                
+                Destroy(Heart[0].gameObject);
+                Destroy(gameObject);
+            }
+            else if (health < 2)
+            {
+                
+                Destroy(Heart[1].gameObject);
+
+            }
+            else if (health < 3)
+            {
+                Destroy(Heart[2].gameObject);
+                
+            }
+        }
+    
 
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-
+        
     }
 }
+
--- Assets/Proto/Code/movement.cs
+++ Assets/Proto/Code/movement.cs
@@ -5,27 +5,39 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class movement : MonoBehaviour
 {
+
     [SerializeField] int Speed;
+
     [SerializeField] int DashSpeed;
+
     [SerializeField] bool isGrounded = false;
+
     [SerializeField] private Rigidbody2D rb;
+
     [SerializeField] int JumpingPower;
+
     [SerializeField] Transform GroundCheckCollider;
+
     [SerializeField] const float GroundCheckRadius = 0.2f;
+
     [SerializeField] LayerMask GroundLayer;
-    // Start is called before the first frame update
+
+    [SerializeField] float NbJump = 2;
+
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         GroundCheck();
         moving();
     }
 
+    //Ground Check who's detecting with a collider if the player is walking on the ground or flying
     void GroundCheck()
     {
         isGrounded = false;
@@ -35,11 +47,13 @@ public class movement : MonoBehaviour
             isGrounded = true;
     }
 
+    //Here for Checking if the ground is colliding with the player or no
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("World"))
         { 
             isGrounded = true;
+            //NbJump = 2;
         }
     }
     void OnCollisionExit2D(Collision2D other)
@@ -52,22 +66,30 @@ public class movement : MonoBehaviour
 
 
 
-
+    //All the movement Code
     void moving()
     {
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+
+        //Jumping
+        if (Input.GetKey(KeyCode.Space) && isGrounded /*|| NbJump > 0*/)
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
+            //NbJump = NbJump - 1;
         }
 
+        //Move Left
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-Speed * Time.deltaTime, 0, 0);
         }
+
+        //Physic Attack 
         if (Input.GetKey(KeyCode.Keypad6))
         {
-           //Attack Phisyc
+            
         }
+
+        //Move Right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Speed * Time.deltaTime, 0, 0);
